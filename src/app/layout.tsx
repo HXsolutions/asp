dedd2aa652ec8d9abecfe86515a6090445739d45
HXsolutions <haxxcelsolutions@gmail.com>
2026-01-14 16:09:44 +0500

    Remove Header and Footer from layout
    Removed Header and Footer components from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,7 @@ export default function RootLayout({
         )}
       >
         <div className="relative flex min-h-screen flex-col">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
+          {children}
         </div>
         <Toaster />
       </body>
